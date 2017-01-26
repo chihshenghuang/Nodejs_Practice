456325f8df14cff1d6c65d9b4ceeadafaf759723
pedoe <pedoe1989@gmail.com>
2017-01-26 04:32:55 +0000

    Add error status if the username or password is wrong

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var isLoginError = false;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next){
-	res.render('index');
+		res.render('index', {title: 'welcome'});
 });
 
-router.get('/index', function(req, res, next) {
+// User login success
+router.get('/success', function(req, res, next) {
+	isLoginError = true;
 	if(req.cookies.user !== null){
-
-	console.log(req.user);
-	res.render('users', req);
+		console.log(req.user);
+		res.render('users', req);
 	}
 });
+
+// User login fail
+router.get('/error', function(req, res, next){
+	if(req.error !== null){
+		console.log("error error");
+		isLoginError = true;
+		req.error = null;
+		res.render('index', {title: 'Wrong', isLoginError: isLoginError});
+	}
+});
+
 
 router.get('/signup', function(req, res, next){
 	res.render('signup', req);
@@ -37,8 +52,10 @@ router.post('/signup', function(req, res, next){
 router.post('/login', function(req, res, next){
 	var db = req.con;
 	var username = req.body.username;
-	var pwd = req.body.pwd;
-	var account = [];
+	var password = req.body.pwd;
+	var username_verify = false,
+	    password_verify = false;
+
 	db.query('SELECT * FROM user', function(err, rows){
 		if(err){
 			console.log(err);
@@ -48,30 +65,38 @@ router.post('/login', function(req, res, next){
 			var items = Object.keys(rows[objectid]);
 			items.forEach(function(itemkey){
 				var itemvalue = rows[objectid][itemkey];
-				if(itemvalue == username)
-					account.push(itemvalue);		
-				if(itemvalue == pwd && account[0] !== null)
-					account.push(itemvalue);
-			})	
+				if((itemkey == 'account') && (itemvalue == username)){
+					username_verify = true;
+					console.log(itemvalue, username_verify);
+				}
+				if((itemkey == 'password') && (itemvalue == password) && username_verify == true){
+					password_verify = true;
+					console.log(itemvalue, password_verify);
+				}
+			})
 		})
-		console.log('user: ' + account[0], 'pwd: ' + account[1]);
+	
+		console.log(username_verify, password_verify);
+		if((username_verify == true) && (password_verify == true)){
+			//Set cookie
+			username_verify = false;
+			password_verify = false;
+			res.cookie("user", username, {maxAge:60000, httpOnly:false});
+			res.redirect('success');
+		}else{
+			console.log("Error user");
+			username_verify = false;
+			password_verify = false;
+			req.error = 'Wrong';
+			res.redirect('error');
+		}
 	});
-		
-	if((account[0] == username) && (account[1] == pwd)){
-		//Set cookie
-		res.cookie("user", username, {maxAge:60000, httpOnly:false});
-		res.redirect('index');
-	}else{
-		console.log("Error user");
-		req.error = "User name or password is wrong";
-		res.render('index', req);
-	}
 });
 
 router.get('/logout', function(req, res, next){
 	console.log('logout');
-	res.clearCookie('user');
-	res.render('index');
+	//res.clearCookie('user');
+	res.render('index', {title: 'welocme'});
 });
 
 module.exports = router;
