695aa766cba28d69ee0d7a677124040db50b4990
pedoe <pedoe1989@gmail.com>
2017-02-07 08:51:32 +0000

    Add viewdata function to show the user information

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 router.get('/viewdata', function(req, res, next){
 	console.log("View user's data");
-	res.send(req.cookies.user);
+	//res.send(req.cookies.user);
+	var db = req.con;
+	
+        db.query('SELECT * FROM users WHERE account = ?', req.cookies.user, function(err, rows){
+                if(err){
+                        console.log(err);
+		}
+		res.render('usersdata', {account: rows[0].account, 
+					password: rows[0].password, 
+					email: rows[0].email,
+					create_time: rows[0].create_time,
+					update_time: rows[0].update_time})
+        });
 });
 
 module.exports = router;
