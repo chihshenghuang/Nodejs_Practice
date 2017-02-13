e02d2a9a4f02f4626eacae9b089186865b7951c3
pedoe <pedoe1989@gmail.com>
2017-02-13 02:05:08 +0000

    Fix the problem that no cookie set will crash the server

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,21 @@ router.get('/viewdata', function(req, res, next){
 	//res.send(req.cookies.user);
 	var db = req.con;
 	
-        db.query('SELECT * FROM users WHERE account = ?', req.cookies.user, function(err, rows){
-                if(err){
-                        console.log(err);
-		}
-		res.render('users', {account: rows[0].account, 
+	if(req.cookies.user){	
+        	db.query('SELECT * FROM users WHERE account = ?', req.cookies.user, function(err, rows){
+                	if(err){
+                        	console.log(err);
+			}
+			res.render('users', {account: rows[0].account, 
 					email: rows[0].email,
 					create_time: rows[0].create_time,
 					update_time: rows[0].update_time})
-        });
+        	});
+	}else{
+		console.log("No user login now!");
+		res.send("No user login now!");
+	}
+
 });
 
 module.exports = router;
