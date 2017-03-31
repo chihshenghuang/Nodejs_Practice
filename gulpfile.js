0e488c0b5bd82b3b65358bf673db4fea5883a647
pedoe <pedoe1989@gmail.com>
2017-03-31 07:36:12 +0000

    Add gulp-uglify for js and catagorize the file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var rename = require('gulp-rename');
+
 
 gulp.task('stylus2css', function buildCSS(){
   return gulp.src('./src/style/*.styl')
   .pipe(stylus())
-  .pipe(gulp.dest('./public/stylesheets/'));
+  .pipe(uglifycss())
+  .pipe(rename(function(path){
+	  	path.basename += ".min";
+		path.extname = ".css";
+  }))
+  .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('default', ['stylus2css']);
+
+gulp.task('uglify', function buildCSS(){
+  return gulp.src('./src/js/*.js')
+  .pipe(uglify())
+  .pipe(rename(function(path){
+	  path.basename += ".min";
+	  path.extname = ".js";
+  }))
+  .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('default', ['stylus2css', 'uglify']);
