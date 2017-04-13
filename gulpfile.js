3c22082ce642a7b3146de7077a92183b635d7466
pedoe <pedoe1989@gmail.com>
2017-04-13 13:33:36 +0000

    Add sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,31 @@ var stylus = require('gulp-stylus');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var rename = require('gulp-rename');
-
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('stylus2css', function buildCSS(){
   return gulp.src('./src/style/*.styl')
+  .pipe(sourcemaps.init())
   .pipe(stylus())
   .pipe(uglifycss())
   .pipe(rename(function(path){
 	  	path.basename += ".min";
 		path.extname = ".css";
   }))
+  .pipe(sourcemaps.write('maps'))
   .pipe(gulp.dest('./public/css/'));
 });
 
 
 gulp.task('uglify', function buildCSS(){
   return gulp.src('./src/js/*.js')
+  .pipe(sourcemaps.init())
   .pipe(uglify())
   .pipe(rename(function(path){
 	  path.basename += ".min";
 	  path.extname = ".js";
   }))
+  .pipe(sourcemaps.write('maps'))
   .pipe(gulp.dest('./public/js/'));
 });
 
